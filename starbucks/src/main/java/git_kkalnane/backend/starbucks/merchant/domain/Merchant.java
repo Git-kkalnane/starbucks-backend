478006baa625c2a0391634f18b9ee8da14172ea6
SeongJun98 <seongjunin98@gmail.com>
2025-06-17 09:55:55 +0900

    feat: merchant entity 작성

---
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/merchant/domain/Merchant.java
@@ -0,0 +1,27 @@
+package git_kkalnane.backend.starbucks.merchant.domain;
+
+import git_kkalnane.backend.starbucks.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "merchant")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Merchant extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "merchantname", nullable = false, length = 50, unique = true)
+    private String merchantname;
+
+    @Column(name = "email", nullable = false, length = 255, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+}
