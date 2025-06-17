6b906f96ff277d488d15be920a727d6d2e27c230
0tae <choiyt3465@naver.com>
2025-06-17 13:59:13 +0900

    feat : Notification 관련 성공 메시지, 에러 메시지, 예외 클래스 정의

---
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/exception/NotificationErrorCode.java
@@ -0,0 +1,27 @@
+package git_kkalnane.backend.starbucks.notification.common.exception;
+
+import git_kkalnane.backend.starbucks.global.error.core.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum NotificationErrorCode implements ErrorCode {
+    ;
+
+    public static final String PREFIX = "[NOTIFICATION ERROR] ";
+
+    private final HttpStatus status;
+    private final String rawMessage;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return PREFIX + rawMessage;
+    }
+}
