7f2f3d6d3498a73667979cb7544c691c535472fc
SeongJun98 <seongjunin98@gmail.com>
2025-06-18 15:09:41 +0900

    feat(store) : 매장 혼잡도 변경 API 삭제

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/store/controller/StoreController.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/store/controller/StoreController.java
@@ -2,14 +2,12 @@ package git_kkalnane.backend.starbucks.store.controller;
 
 import git_kkalnane.backend.starbucks.global.success.SuccessResponse;
 import git_kkalnane.backend.starbucks.store.common.success.StoreSuccessCode;
-import git_kkalnane.backend.starbucks.store.dto.request.UpdateCrowdLevelRequest;
 import git_kkalnane.backend.starbucks.store.dto.response.StoreDetailsResponse;
 import git_kkalnane.backend.starbucks.store.service.StoreService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,22 +38,4 @@ public class StoreController {
         return ResponseEntity.ok(SuccessResponse.of(StoreSuccessCode.STORE_DETAIL_RETRIEVED, response));
     }
 
-    @Operation(
-            summary = "지점 혼잡도 변경",
-            description = "특정 지점의 현재 혼잡도 수준을 업데이트. 새로운 혼잡도 레벨을 요청 본문으로 받습니다."
-    )
-    @ApiResponses(value ={
-            @ApiResponse(responseCode = "200", description = "지점 혼잡도 변경 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-            @ApiResponse(responseCode = "404", description = "해당 ID의 지점을 찾을 수 없음")
-    })
-
-    @PutMapping("/{storeId}/crowd-level")
-    public ResponseEntity<SuccessResponse<StoreDetailsResponse>> updateStoreCrowdLevel(
-            @PathVariable Long storeId,
-            @Valid @RequestBody UpdateCrowdLevelRequest request) {
-        StoreDetailsResponse response = storeService.updateCrowdLevel(storeId, request.getNewCrowdLevel());
-
-        return ResponseEntity.ok(SuccessResponse.of(StoreSuccessCode.CROWD_LEVEL_UPDATED, response));
-    }
 }
