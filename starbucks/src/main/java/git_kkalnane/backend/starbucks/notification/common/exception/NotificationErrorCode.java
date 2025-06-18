ac868b937021405524cd7970eef73bd7721642d3
0tae <choiyt3465@naver.com>
2025-06-18 18:02:01 +0900

    feat : 알림 전송 관련 성공, 에러코드 정의

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/exception/NotificationErrorCode.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/exception/NotificationErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum NotificationErrorCode implements ErrorCode {
+    INVALID_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 알림입니다. 알림 타입을 확인해주세요. 알림 타입: %s"),
     ;
 
     public static final String PREFIX = "[NOTIFICATION ERROR] ";
--- starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/success/NotificationSuccessCode.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/success/NotificationSuccessCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum NotificationSuccessCode implements SuccessCode {
     NOTIFICATION_SUBSCRIBED(HttpStatus.CREATED, "알림 구독에 성공하였습니다. %s"),
-    NOTIFICATION_SUBSCRIPTION_RETRIEVED(HttpStatus.OK, "알림 구독 목록이 성공적으로 조회되었습니다.")
+    NOTIFICATION_SUBSCRIPTION_RETRIEVED(HttpStatus.OK, "알림 구독 목록이 성공적으로 조회되었습니다."),
+    NOTIFICATION_DELIVERED(HttpStatus.OK, "알림이 성공적으로 전송되었습니다.")
     ;
 
     private final HttpStatus status;
