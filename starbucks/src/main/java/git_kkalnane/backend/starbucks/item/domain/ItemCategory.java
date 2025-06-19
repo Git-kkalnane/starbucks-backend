c215433ec120536f94d2d6a459d0d99875926b36
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-19 15:12:31 +0900

    refactor: 변경된 경로 적용

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/item/domain/ItemCategory.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/item/domain/ItemCategory.java
@@ -1,6 +1,8 @@
     package git_kkalnane.backend.starbucks.item.domain;
 
     import git_kkalnane.backend.starbucks._global.entity.BaseTimeEntity;
+    import git_kkalnane.backend.starbucks.item.domain.beverage.BeverageItemCategory;
+    import git_kkalnane.backend.starbucks.item.domain.dessert.DessertItemCategory;
     import jakarta.persistence.*;
     import lombok.AccessLevel;
     import lombok.Getter;
