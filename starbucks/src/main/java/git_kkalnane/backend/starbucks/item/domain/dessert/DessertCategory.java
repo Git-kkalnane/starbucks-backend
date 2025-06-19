e597cb03f7cddc29cba56d2f5d90e2b81b75fac2
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-19 15:12:05 +0900

    refactor: dessert관련 .java 파일 이동

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/item/domain/dessert/DessertCategory.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/item/domain/dessert/DessertCategory.java
@@ -1,4 +1,4 @@
-package git_kkalnane.backend.starbucks.item.domain;
+package git_kkalnane.backend.starbucks.item.domain.dessert;
 
 import git_kkalnane.backend.starbucks._global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
