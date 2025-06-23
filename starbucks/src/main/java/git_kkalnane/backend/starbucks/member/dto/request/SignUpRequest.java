a09541db4ebbb9b7835504d7b32e01d11b710da4
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-23 15:42:36 +0900

    refactor: DTO 레코드로 변경
    SignUpRequest와 SignUpResponse를 레코드로 변경

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/request/SignUpRequest.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/request/SignUpRequest.java
@@ -1,17 +1,11 @@
 package git_kkalnane.backend.starbucks.member.dto.request;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@Getter
-@ToString
-public class SignUpRequest {
-
-    private String name;
-    private String nickname;
-    private String email;
-    private String password;
+/**
+ * 회원가입 요청 시 프론트엔드 측에서 넘어오는 데이터를 담기 위한 DTO
+ * @param name 가입자 이름
+ * @param nickname 가입자 닉네임
+ * @param email 가입자 이메일
+ * @param password 가입자 비밀번호
+ */
+public record SignUpRequest(String name, String nickname, String email, String password) {
 }
--- starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/response/SignUpResponse.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/response/SignUpResponse.java
@@ -1,13 +1,8 @@
 package git_kkalnane.backend.starbucks.member.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@Getter
-@ToString
-public class SignUpResponse {
-
-    private String name;
+/**
+ * 회원가입 응답 시 프론트엔드 측으로 넘겨줄 데이터를 담기위한 DTO
+ * @param name 가입자 이름
+ */
+public record SignUpResponse(String name) {
 }
