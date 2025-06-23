726174bd5810f598f51477d1a41079d453466a47
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-23 11:36:44 +0900

    feat(AuthController.java): 로그인 컨트롤러 레이어 구현
    - 클라이언트로부터 온 로그인 요청(`/api/v1/auth/login`)을 처리하는 login 메서드를 구현하였다.

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/auth/controller/AuthController.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/auth/controller/AuthController.java
@@ -1,15 +1,25 @@
 package git_kkalnane.backend.starbucks.auth.controller;
 
+import git_kkalnane.backend.starbucks._global.success.SuccessResponse;
 import git_kkalnane.backend.starbucks.auth.common.jwt.JwtToken;
 import git_kkalnane.backend.starbucks.auth.common.success.AuthSuccessCode;
+import git_kkalnane.backend.starbucks.auth.dto.LoginDto;
+import git_kkalnane.backend.starbucks.auth.dto.UserInfo;
+import git_kkalnane.backend.starbucks.auth.dto.request.LoginRequest;
+import git_kkalnane.backend.starbucks.auth.dto.response.LoginResponse;
 import git_kkalnane.backend.starbucks.auth.service.AuthService;
-import git_kkalnane.backend.starbucks._global.success.SuccessResponse;
+import git_kkalnane.backend.starbucks.auth.utils.CookieGenerator;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,23 +31,37 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/create-token")
     @Operation(
-            summary = "JWT 토큰 생성",
-            description = "회원 가입 완료 후 JWT 토큰을 발급합니다."
+            summary = "로그인",
+            description = "로그인 시 사용하는 API"
     )
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "토큰 생성 성공"
+                    description = "로그인 성공"
             ),
             @ApiResponse(
-                    responseCode = "404",
-                    description = "멤버 아이디가 존재하지 않음"
-            ),
+                    responseCode = "400",
+                    description = "올바르지 않은 이메일 또는 패스워드"
+            )
     })
-    public ResponseEntity<SuccessResponse<JwtToken>> createToken() {
-        JwtToken token = authService.createToken(1L);
-        return ResponseEntity.ok((SuccessResponse.of(AuthSuccessCode.SIGN_UP_COMPLETED,token)));
+    @Parameters({
+            @Parameter(name = "email", description = "회원 이메일", example = "user0123@gmail.com"),
+            @Parameter(name = "password", description = "비밀번호", example = "password0123")
+    })
+    @PostMapping("/login")
+    public ResponseEntity<SuccessResponse<LoginResponse>> login(@RequestBody LoginRequest request) {
+
+        LoginDto loginDto = authService.login(request);
+
+        JwtToken token = loginDto.token();
+        UserInfo userInfo = loginDto.userInfo();
+
+        ResponseCookie responseCookie = CookieGenerator.createRefreshTokenCookie(token.getRefreshToken());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .body(SuccessResponse.of(AuthSuccessCode.LOGIN_COMPLETED,
+                        LoginResponse.of(token.getAccessToken(), userInfo)));
     }
 }
