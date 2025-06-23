8baa879b6553699ddf288cdfbc03d3c50dac1570
dotae1 <9636515@gmail.com>
2025-06-24 00:21:22 +0900

    import문 제거

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/cart/dto/request/CartAddItemRequest.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/cart/dto/request/CartAddItemRequest.java
@@ -1,10 +1,5 @@
 package git_kkalnane.backend.starbucks.cart.dto.request;
 
-import git_kkalnane.backend.starbucks.item.domain.ItemOption;
-import git_kkalnane.backend.starbucks.item.domain.ItemType;
-import git_kkalnane.backend.starbucks.item.domain.beverage.enums.BeverageSizeOption;
-import git_kkalnane.backend.starbucks.item.domain.beverage.enums.BeverageTemperatureOption;
-
 import java.util.List;
 
 public record CartAddItemRequest(
--- starbucks/src/main/java/git_kkalnane/backend/starbucks/cart/dto/request/CartItemOptionRequest.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/cart/dto/request/CartItemOptionRequest.java
@@ -2,7 +2,6 @@ package git_kkalnane.backend.starbucks.cart.dto.request;
 
 import git_kkalnane.backend.starbucks.item.domain.ItemType;
 
-
 public record CartItemOptionRequest(
 
         Long itemOptionId,
