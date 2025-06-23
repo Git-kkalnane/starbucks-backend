0bb98c0e90e796e820fbcc7041585072cab14cff
0tae <choiyt3465@naver.com>
2025-06-23 10:15:39 +0900

    feat : findAllByReceiverId 쿼리 메소드 추가

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/repository/NotificationRepository.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/repository/NotificationRepository.java
@@ -1,7 +1,10 @@
 package git_kkalnane.backend.starbucks.notification.repository;
 
 import git_kkalnane.backend.starbucks.notification.domain.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
+    Page<Notification> findAllByReceiverId(Long receiverId, Pageable pageable);
 }
