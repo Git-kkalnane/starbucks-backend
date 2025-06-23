99e10c03185e1d0b1845e9c32a183dca49e63996
0tae <choiyt3465@naver.com>
2025-06-23 10:15:06 +0900

    feat : 알림 목록 조회 성공 코드 정의

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/success/NotificationSuccessCode.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/common/success/NotificationSuccessCode.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 public enum NotificationSuccessCode implements SuccessCode {
     NOTIFICATION_SUBSCRIBED(HttpStatus.CREATED, "알림 구독에 성공하였습니다. %s"),
     NOTIFICATION_SUBSCRIPTION_RETRIEVED(HttpStatus.OK, "알림 구독 목록이 성공적으로 조회되었습니다."),
+    NOTIFICATION_RETRIEVED(HttpStatus.OK, "알림 목록이 성공적으로 조회되었습니다."),
     NOTIFICATION_DELIVERED(HttpStatus.OK, "알림이 성공적으로 전송되었습니다.")
     ;
 
