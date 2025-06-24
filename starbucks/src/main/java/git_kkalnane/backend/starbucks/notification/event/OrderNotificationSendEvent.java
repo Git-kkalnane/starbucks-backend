a47e2b5b2ac6afe5665d18a9d7529a74c20c5941
0tae <choiyt3465@naver.com>
2025-06-24 16:47:27 +0900

    refactor : 멤버 필드에 orderId가 아닌 Order를 포함하도록 변경

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/event/OrderNotificationSendEvent.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/notification/event/OrderNotificationSendEvent.java
@@ -4,13 +4,14 @@ import git_kkalnane.backend.starbucks.notification.domain.NotificationTargetType
 import git_kkalnane.backend.starbucks.notification.domain.NotificationType;
 import git_kkalnane.backend.starbucks.notification.domain.vo.NotificationReceiver;
 import git_kkalnane.backend.starbucks.notification.domain.vo.NotificationSender;
+import git_kkalnane.backend.starbucks.order.domain.Order;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
 public class OrderNotificationSendEvent extends ApplicationEvent {
 
-    private Long orderId;
+    private Order order;
     private String title;
     private String message;
     private NotificationSender sender;
@@ -19,14 +20,14 @@ public class OrderNotificationSendEvent extends ApplicationEvent {
     private NotificationType notificationType;
 
     public OrderNotificationSendEvent(Object object,
-                                      Long orderId,
+                                      Order order,
                                       NotificationSender sender,
                                       NotificationReceiver receiver,
                                       NotificationType notificationType, NotificationTargetType notificationTargetType) {
         super(object);
         this.title = notificationType.getTitle();
         this.message = notificationType.getMessage();
-        this.orderId = orderId;
+        this.order = order;
         this.sender = sender;
         this.receiver = receiver;
         this.notificationType = notificationType;
