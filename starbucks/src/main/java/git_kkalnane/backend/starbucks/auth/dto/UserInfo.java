814db08eba3724d118457ca29bf38ef6a099f212
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-25 14:42:05 +0900

    refactor: UserInfo DTO 삭제

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/auth/dto/UserInfo.java
+++
@@ -1,8 +0,0 @@
-package git_kkalnane.backend.starbucks.auth.dto;
-
-public record UserInfo(String email, String nickname) {
-
-    public static UserInfo of(String email, String nickname) {
-        return new UserInfo(email, nickname);
-    }
-}
