29abca1690676055dc63d67050fd939e83ef0cb6
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-25 10:47:57 +0900

    feat(AuthInterceptor.java): 인터셉터 내부 코드 수정 및 기능 활성화
    - `AuthInterceptor`에 존재하던 코드를 활성화하여 `WebMvcConfigurer`의 `addInterceptors` 메서드에서 인터셉터가 적용될 URI에 접근하기 전 인증 여부를 검증하게 하였다.
    - HTTP Authorization 헤더에 헤더가 없거나 또는 값이 비어있는 경우를 검증하는 메서드 `validateBearerToken` 메서드를 구현하였다.
    - HTTP Authorization 헤더에 포함된 액세스 토큰에서 접두사(Bearer)를 제거하는 기능은 정적 유틸리티 클래스 `TokenParser`로 이동하였으므로 `extractToken` 메서드를 삭제하였다.
    - 액세스 토큰에 대한 유효성을 검증하는 `verifyTokenIncludedInRequest` 메서드를 `AuthService`에서 호출하는 구문을 추가했다.
    - 그 외 GlobalLogger에서 사용되는 리터럴 문자를 별도의 상수로 분리하였다.

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/_global/config/AuthInterceptor.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/_global/config/AuthInterceptor.java
@@ -1,43 +1,61 @@
 package git_kkalnane.backend.starbucks._global.config;
 
+import git_kkalnane.backend.starbucks._global.utils.GlobalLogger;
 import git_kkalnane.backend.starbucks.auth.common.exception.AuthErrorCode;
 import git_kkalnane.backend.starbucks.auth.common.exception.AuthException;
+import git_kkalnane.backend.starbucks.auth.common.jwt.JwtTokenProvider;
+import git_kkalnane.backend.starbucks.auth.common.jwt.utils.TokenParser;
 import git_kkalnane.backend.starbucks.auth.service.AuthService;
-import git_kkalnane.backend.starbucks._global.utils.GlobalLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
 public class AuthInterceptor implements HandlerInterceptor {
-    private final AuthService authInfoService;
+
+    private static final String REQUEST_URI_LOG_PREFIX = "[Intercept] 요청 경로 정보: ";
+    private static final String MEMBER_ID_LOG_PREFIX = "[Intercept] memberId: ";
+
+    private final AuthService authService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        GlobalLogger.info("[Intercept] 요청 경로 정보:",request.getMethod(),request.getRequestURI());
 
-//      TODO: 기능 구현 시 아래 코드 활성화
-//        String accessToken = extractToken(request);
-//        AuthInfo authInfo = authInfoService.verify(accessToken);
+        GlobalLogger.info(REQUEST_URI_LOG_PREFIX, request.getMethod(), request.getRequestURI());
 
-        Long memberId =  1L; // authInfo.getMemberId();
+        // HTTP 요청의 Authorization 헤더의 값을 가져온다.
+        String bearerToken = request.getHeader(JwtTokenProvider.ACCESS_HEADER_STRING);
+        validateBearerToken(bearerToken);
 
+        // Authorization 헤더의 값에서 접두사(Bearer)를 제거한다.
+        String accessToken = TokenParser.removeBearerTokenPrefix(bearerToken);
+
+        // 액세스 토큰 유효성 검증
+        Long memberId = authService.verifyTokenIncludedInRequest(accessToken);
+
+        // HTTP 요청의 속성에 멤버 엔티티의 식별자를 추가한다.
         request.setAttribute("memberId", memberId);
 
-        GlobalLogger.info("[Intercept] memberId: ",memberId);
+        GlobalLogger.info(MEMBER_ID_LOG_PREFIX, memberId);
+
         return true;
     }
 
-    private String extractToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
+    /**
+     * Bearer 토큰의 값을 검증하는 메서드
+     * <br><br>
+     * Authorization 헤더에 있는 accessToken이 null이거나 빈 문자열인지 검증한다.
+     *
+     * @param bearerToken Authorization 헤더에 있는 bearerToken
+     */
+    private void validateBearerToken(String bearerToken) {
 
-        throw new AuthException(AuthErrorCode.INVALID_TOKEN);
+        // 토큰이 존재하지 않거나 빈 문자열일 경우 401 에러코드 반환
+        if (bearerToken == null || bearerToken.isBlank()) {
+            throw new AuthException(AuthErrorCode.UNAUTHORIZED_ACCESS);
+        }
     }
 }
