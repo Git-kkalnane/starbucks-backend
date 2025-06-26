f4646ed8edc07c8bb0f6508d63114d59ecec0810
0tae <choiyt3465@naver.com>
2025-06-26 16:55:06 +0900

    refactor : PayCard 테스트 결과에 따른 코드 수정

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/paycard/common/exception/PayCardErrorCode.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/paycard/common/exception/PayCardErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum PayCardErrorCode implements ErrorCode {
     PAY_CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "결제 카드가 존재하지 않습니다."),
-    NOT_ENOUGH_PAY_CARD_AMOUNT(HttpStatus.BAD_REQUEST, "결제 카드의 잔액이 부족합니다. 요청 금액: %s, 현재 잔액: %s"),
+    NOT_ENOUGH_PAY_CARD_AMOUNT(HttpStatus.BAD_REQUEST, "결제 카드의 잔액이 부족합니다."),
     ;
 
     public static final String PREFIX = "[PAY_CARD ERROR] ";
