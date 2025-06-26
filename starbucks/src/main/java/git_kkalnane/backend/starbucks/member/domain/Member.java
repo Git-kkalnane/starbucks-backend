64a049910469f115dad881609493b03320e5830b
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-26 22:42:44 +0900

    feat(Member.java): Member 엔티티의 컬럼들에 NOT NULL 제약조건 추가
    엔티티의 컬럼들에 nullable = false를 추가하여 엔티티에 null 값이 들어올 수 없도록 설정했다.

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/member/domain/Member.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/member/domain/Member.java
@@ -29,16 +29,16 @@ public class Member extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "member_name")
+    @Column(name = "member_name", nullable = false)
     private String name;
 
-    @Column(name = "nickname")
+    @Column(name = "nickname", nullable = false)
     private String nickname;
 
-    @Column(name = "email", unique = true)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "password_hash")
+    @Column(name = "password_hash", nullable = false)
     private String password;
 
     @Builder
