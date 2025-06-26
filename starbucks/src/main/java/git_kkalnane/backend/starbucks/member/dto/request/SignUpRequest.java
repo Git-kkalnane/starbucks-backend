b220a7185a167179f0e3f7d39508a2a44b180ba8
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-26 22:40:58 +0900

    feat(SignUpRequest.java): DTO의 필드들에 Validation 어노테이션 추가
    DTO의 필드들에 이전에 구현한 Validation 어노테이션을 추가했다.

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/request/SignUpRequest.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/request/SignUpRequest.java
@@ -1,11 +1,29 @@
 package git_kkalnane.backend.starbucks.member.dto.request;
 
+import git_kkalnane.backend.starbucks.member.validation.annotation.ValidEmail;
+import git_kkalnane.backend.starbucks.member.validation.annotation.ValidName;
+import git_kkalnane.backend.starbucks.member.validation.annotation.ValidNickname;
+import git_kkalnane.backend.starbucks.member.validation.annotation.ValidPassword;
+
 /**
  * 회원가입 요청 시 프론트엔드 측에서 넘어오는 데이터를 담기 위한 DTO
- * @param name 가입자 이름
+ *
+ * @param name     가입자 이름
  * @param nickname 가입자 닉네임
- * @param email 가입자 이메일
+ * @param email    가입자 이메일
  * @param password 가입자 비밀번호
  */
-public record SignUpRequest(String name, String nickname, String email, String password) {
+public record SignUpRequest(
+
+        @ValidName
+        String name,
+
+        @ValidNickname
+        String nickname,
+
+        @ValidEmail
+        String email,
+
+        @ValidPassword
+        String password) {
 }
