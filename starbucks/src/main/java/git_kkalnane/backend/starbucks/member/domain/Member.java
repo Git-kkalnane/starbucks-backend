8d03db26e8c6a6bba0b712887fd5db4ec798dd8e
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-26 23:20:57 +0900

    chore(Member.java): 불필요한 import문 제거

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/member/domain/Member.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/member/domain/Member.java
@@ -1,8 +1,6 @@
 package git_kkalnane.backend.starbucks.member.domain;
 
 import git_kkalnane.backend.starbucks._global.entity.BaseTimeEntity;
-import git_kkalnane.backend.starbucks.member.dto.request.SignUpRequest;
-import jakarta.persistence.*;
 import lombok.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
