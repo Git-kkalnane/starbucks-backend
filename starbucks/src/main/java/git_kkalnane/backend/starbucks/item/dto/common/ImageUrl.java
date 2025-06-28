152898329f266d823a39f0651037b8512fa5e01f
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-28 15:31:11 +0900

    refactor(ImageUrl): 필드 이름 변경

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/item/dto/common/ImageUrl.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/item/dto/common/ImageUrl.java
@@ -5,12 +5,12 @@ import git_kkalnane.backend.starbucks.item.domain.beverage.BeverageItem;
 /**
  * 이미지 URL 정보를 담는 레코드
  *
- * @param hotImageUrl 핫 음료 이미지 URL
- * @param iceImageUrl 아이스 음료 이미지 URL
+ * @param hot 핫 음료 이미지 URL
+ * @param ice 아이스 음료 이미지 URL
  */
 public record ImageUrl(
-    String hotImageUrl,
-    String iceImageUrl
+    String hot,
+    String ice
 ) {
   public static ImageUrl from(BeverageItem beverageItem) {
     return new ImageUrl(
