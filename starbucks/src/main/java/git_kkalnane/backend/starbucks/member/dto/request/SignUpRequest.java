d023a391f23d1a0a479354ba0ef1fed93e0c5e20
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-30 14:17:36 +0900

    refactor: Validation 패키지 변경에 따른 import문 수정

--- starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/request/SignUpRequest.java
+++ starbucks/src/main/java/git_kkalnane/backend/starbucks/member/dto/request/SignUpRequest.java
@@ -1,9 +1,9 @@
 package git_kkalnane.backend.starbucks.member.dto.request;
 
-import git_kkalnane.backend.starbucks.member.validation.annotation.ValidEmail;
-import git_kkalnane.backend.starbucks.member.validation.annotation.ValidName;
-import git_kkalnane.backend.starbucks.member.validation.annotation.ValidNickname;
-import git_kkalnane.backend.starbucks.member.validation.annotation.ValidPassword;
+import git_kkalnane.backend.starbucks._global.validation.annotation.ValidEmail;
+import git_kkalnane.backend.starbucks._global.validation.annotation.ValidName;
+import git_kkalnane.backend.starbucks._global.validation.annotation.ValidNickname;
+import git_kkalnane.backend.starbucks._global.validation.annotation.ValidPassword;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
